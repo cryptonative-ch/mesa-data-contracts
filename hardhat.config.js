bc614dae75f03476eb3bffa4ee9e45197c82c22b
Adam Azad <adam@adamazad.com>
2021-03-11 23:53:22 +0300

    Add Rinkeby network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
+require('@nomiclabs/hardhat-ethers')
+const { mnemonic, infuraKey } = require('./secrets.json')
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.6.8',
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraKey}`,
+      gasPrice: 45000000000,
+      gas: 3200000,
+      accounts: { mnemonic },
+    },
+  },
 }
